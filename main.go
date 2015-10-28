89f012608decfe78f821f257ccbe28135e87fe1c
Shawn Milochik <shawn@milochik.com>
2015-10-28 13:10:16 -0400

    added first shortcut!

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -18,6 +19,23 @@ func main() {
 
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		fmt.Println(s.Text())
+		t := s.Text()
+		line := strings.TrimSpace(s.Text())
+		if strings.HasPrefix(line, "nnf:") {
+			printNotNilFatal(t)
+		} else {
+			fmt.Println(t)
+		}
 	}
+}
+
+func printNotNilFatal(line string) {
+	l := len(line) - len(strings.TrimLeft(line, " "))
+	pad := strings.Repeat(" ", l)
+	msg := strings.SplitN(line, ":", 2)[1]
+
+	// print error block
+	fmt.Printf("%sif err != nil{\n", pad)
+	fmt.Printf("%s%slog.Fatal(%q, err)\n", pad, pad, msg+": ")
+	fmt.Printf("%s}\n", pad)
 }
