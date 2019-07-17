88d871d4e2ae6f2ea13d00428128f33c67d91a3a
Shawn Milochik <shawn@milochik.com>
2019-07-17 14:16:34 -0400

    Add some stuff, remove DOM stuff

--- main.go
+++ main.go
@@ -27,6 +27,10 @@ var replace = map[string]func(){
 	"gomain":    goMain,
 	"flagsh":    flagsh,
 	"dummyType": dummyType,
+	"jm":        jsonMarshal,
+	"ju":        jsonUnmarshal,
+	"rb":        readBody,
+	"watcher":   watcher,
 }
 
 var update = map[string]func(string){
@@ -38,8 +42,6 @@ var update = map[string]func(string){
 	"ow:":   pyOpenWrite,
 	"ul":    ul,
 }
-
-var dom = []string{"div", "form", "section", "article", "body", "header"}
 
 func main() {
 	if os.Getenv("shortcuts") == "off" {
@@ -69,14 +71,6 @@ func main() {
 		for pre := range update {
 			if strings.HasPrefix(trim, pre) {
 				update[pre](line)
-				replaced = true
-				break 
-			}
-
-		}
-		for _, pre := range dom {
-			if strings.HasPrefix(trim, pre) {
-				element(line, pre)
 				replaced = true
 				break
 			}
@@ -177,7 +171,8 @@ func html5() {
         <link rel="stylesheet" href="https://aoeus.com/milligram.min.css" type="text/css">
 		<meta name="viewport" content="width-device-width, initial-scale=1">
         <style type="text/css">
-            body{
+            <body>
+            </body>
                 margin: 40px auto;
                 max-width: 650px;
                 line-height: 1.6;
@@ -243,7 +238,7 @@ func ul(line string) {
 
 }
 
-func element(line , el string) {
+func element(line, el string) {
 	margin := len(line) - len(strings.TrimLeft(line, " \t"))
 	padding := line[0:margin]
 	fmt.Printf(padding)
@@ -352,4 +347,57 @@ func dummyType() {
     color string
 }
 `)
+}
+func jsonMarshal() {
+	fmt.Println(`b, err := json.Marshal(x)
+    if err != nil{
+        log.Fatalf("Failed to do something: %s\n", err)
+	} `)
+}
+
+func jsonUnmarshal() {
+	fmt.Println(`var x thing
+err = json.Unmarshal(b, &x)
+return x, err
+`)
+}
+
+func readBody() {
+	fmt.Println(`b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()`)
+}
+
+func watcher() {
+	fmt.Println(`#!/usr/bin/env bash
+
+bin=monkey
+flag=$(mktemp)
+code=main.go
+
+function cleanup() {
+    pkill $bin
+    rm -f $bin
+    rm -f $flag
+    exit
+}
+
+trap cleanup SIGINT SIGTERM
+
+while true
+do
+    if [ ! "$code" -nt $flag ]; then
+        sleep 5
+        continue
+    fi
+    go build -o $bin $code || { touch $flag; continue; }
+    pkill $bin
+    ./$bin &
+    sleep 1
+    ./tests.sh
+    touch $flag
+    sleep 15
+done`)
 }
